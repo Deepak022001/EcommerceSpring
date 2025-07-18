c4aaa002435bb00e0976ea6e49feaebc8d0d9acf
Deepak singh <studydipu@gmail.com>
2025-07-19 00:38:27 +0530

    Initial commit

---
+++ src/main/java/org/example/EcommerceSpring/EcommerceSpringApplication.java
@@ -0,0 +1,15 @@
+package org.example.EcommerceSpring;
+
+import io.github.cdimascio.dotenv.Dotenv;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceSpringApplication {
+	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.configure().load();
+		dotenv.entries().forEach(e -> System.setProperty(e.getKey(), e.getValue()));
+		System.getProperty("PORT");
+		SpringApplication.run(EcommerceSpringApplication.class, args);
+	}
+}
